9453554d86a53a55ca95c49e4d367d5cc7a3acf6
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2020-03-04 11:21:13 -0500

    working on cuke tests

--- features/step_definitions/upload_steps.rb
+++ features/step_definitions/upload_steps.rb
@@ -1,13 +1,26 @@
 require 'uri'
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
+
+#uses rspec-expectations gem
+
 Given /^(?:I) am on the (.+)$/ do |page_name|
     visit path_to(page_name) 
 
 end
 
-When /^(?:I) upload a (.+) file$/ do |file_type|
-    #Creating an entry in the 
+
+
+When /^(?:I) upload a (.+) file called "(.+)"$/ do |file_type, file_name|
+    attach_file('data-file', 'app/assets/Uploads/'+file_name)
+    
+
 end
 
-Then 
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
