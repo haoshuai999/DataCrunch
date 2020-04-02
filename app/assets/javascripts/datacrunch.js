b701727f0674cc956e7a34bdfa90f82a06a1a996
haoshuai999 <626593718@qq.com>
2020-04-02 17:04:51 -0400

    data viz for each continuous column

--- app/assets/javascripts/datacrunch.js
+++ app/assets/javascripts/datacrunch.js
@@ -1,11 +1,11 @@
-var loadData = function(){
+var loadData = function(column_name){
     $.ajax({
       type: 'GET',
       contentType: 'application/json; charset=utf-8',
       url: window.location.pathname,
       dataType: 'text',
       success: function(data){
-        drawHistogram(data);
+        drawHistogram(data, column_name);
       },
       failure: function(result){
         error();
@@ -22,6 +22,12 @@ function error() {
 
 // fetch data on page load
 $(document).ready(function(){
-    console.log("called");
-    loadData(); 
+    $("tr:nth-child(2) td").each(function(i){
+      $(this).on("click", function(){
+        $("svg").remove();
+        var column_name = $("tr:nth-child(2) td:nth-child("+ (i+1) + ") a").text();
+        console.log(i);
+        loadData(column_name); 
+      });
+    });
 });
