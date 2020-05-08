23ef73714cba70c8cd40b5c8ee4207c045fefe65
haoshuai999 <626593718@qq.com>
2020-05-08 01:17:18 -0400

    finish dataviz test for JSON files

--- spec/dataviz_spec.rb
+++ spec/dataviz_spec.rb
@@ -97,7 +97,7 @@ describe DatacrunchesController do
 
         context 'Large JSON file with missing values' do
             before :each do
-                @file_path = File.join Rails.root, 'spec/fixtures/files/data.xlsx'
+                @file_path = File.join Rails.root, 'spec/fixtures/files/data.json'
                 json_file = JSON.parse(File.open(@file_path).read)
                 json_converter = JsonConverter.new
                 csv_json = json_converter.generate_csv(json_file)
@@ -107,41 +107,42 @@ describe DatacrunchesController do
                 @dataframe = Daru::DataFrame.rows(csv.to_a[1..-1], order: csv.headers)
             end
 
-            # it 'convert continuous vector to json' do
-            #     @columnvector = @dataframe["num_comments"]
-            #     @continuous = Datacrunch.process_continous(@columnvector)
-            #     expect(@continuous).to be_an_instance_of(Array)
-            #     expect(@continuous[0][:num_comments]).to eq(7)
-            #     expect(@continuous.length).to eq(259)
-            # end
+            it 'convert continuous vector to json' do
+                @columnvector = @dataframe["Funding.Request"]
+                @continuous = Datacrunch.process_continous(@columnvector)
+                expect(@continuous).to be_an_instance_of(Array)
+                expect(@continuous[0][:Funding][:Request]).to eq(70156877)
+                expect(@continuous.length).to eq(15)
+            end
 
-            # it 'categorical data with more than 10 unique values' do
-            #     @columnvector = @dataframe["author"]
-            #     @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
-            #     expect(@categorical).to be_an_instance_of(Array)
-            #     expect(@categorical[0][:freq]).to eq(4)
-            #     expect(@categorical[0][:column]).to eq("Blulien")
-            #     expect(@categorical[-1][:freq]).to eq(230)
-            #     expect(@categorical[-1][:column]).to eq("Other values")
-            #     expect(@categorical.length).to eq(11)
-            # end
+            it 'categorical data with more than 10 unique values' do
+                @columnvector = @dataframe["Applicant.Name"]
+                @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
+                expect(@categorical).to be_an_instance_of(Array)
+                expect(@categorical[0][:freq]).to eq(2)
+                expect(@categorical[0][:column]).to eq("Florida Department of Transportation")
+                expect(@categorical[-1][:freq]).to eq(4)
+                expect(@categorical[-1][:column]).to eq("Other values")
+                expect(@categorical.length).to eq(11)
+            end
 
-            # it 'categorical data with five unique values' do
-            #     @columnvector = @dataframe["subreddit"]
-            #     @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
-            #     expect(@categorical).to be_an_instance_of(Array)
-            #     expect(@categorical[-1][:column]).not_to eq("Other values")
-            #     expect(@categorical.length).to eq(5)
-            # end
+            it 'categorical data with two unique values' do
+                @columnvector = @dataframe["Awardee"]
+                @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
+                expect(@categorical).to be_an_instance_of(Array)
+                expect(@categorical[-1][:column]).not_to eq("Other values")
+                expect(@categorical.length).to eq(2)
+            end
 
-            # it 'categorical data with missing values' do
-            #     @columnvector = @dataframe["is_self"]
-            #     @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
-            #     expect(@categorical).to be_an_instance_of(Array)
-            #     expect(@categorical[1][:freq]).to eq(5)
-            #     expect(@categorical[1][:column]).to eq(:null)
-            #     expect(@categorical.length).to eq(2)
-            # end
+            it 'categorical data with missing values' do
+                @columnvector = @dataframe["President"]
+                @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
+                expect(@categorical).to be_an_instance_of(Array)
+                expect(@categorical[-1][:freq]).to eq(1)
+                # The following test is not ideal, because the label for missing values should be null
+                expect(@categorical[-1][:column]).to eq("")
+                expect(@categorical.length).to eq(3)
+            end
         end
     end
 end
