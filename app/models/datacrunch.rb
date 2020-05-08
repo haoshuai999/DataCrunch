2bf8cb9d18899d791d46523c4a1ffc090b5519d7
haoshuai999 <626593718@qq.com>
2020-05-07 23:31:02 -0400

    expand the first test

--- app/models/datacrunch.rb
+++ app/models/datacrunch.rb
@@ -6,13 +6,15 @@ class Datacrunch < ActiveRecord::Base
     Paperclip.options[:content_type_mappings] = {:csv => "text/plain", :json => "text/plain"}
 
     def self.process_categorical(dataframe, vector)
-        counter_obj = Counter.new(vector.to_a).most_common(10).to_h
+        displayed_columns = 10
+        counter_obj = Counter.new(vector.to_a).most_common(displayed_columns).to_h
         if vector.size - vector.count > 0
             counter_obj[:null] = counter_obj.delete nil
+            displayed_columns -= 1
         end
 
         counter_df = Daru::DataFrame.new({:column => counter_obj.keys, :freq => counter_obj.values})
-        if Counter.new(vector.to_a).keys.length > 10
+        if Counter.new(vector.to_a).keys.length > displayed_columns
             other_value = dataframe.nrows - counter_df[:freq].sum
             counter_df.add_row(["Other values", other_value])
         end
--- spec/dataviz_spec.rb
+++ spec/dataviz_spec.rb
@@ -1,27 +1,39 @@
 require 'rails_helper'
 
 describe DatacrunchesController do
-    describe 'show continuous data using histogram' do
-        context 'no missing data' do
-            before :each do
-                @file_path = File.join Rails.root, 'spec/fixtures/files/wine-data.csv'
-                # @datacrunch = FactoryGirl.create(:datacrunch)
-                @dataframe = Daru::DataFrame.read_csv(@file_path, liberal_parsing: true)
-                @columnvector = @dataframe["points"]
-            end
-    
-            it 'convert vector to json' do
-                @continuous = Datacrunch.process_continous(@columnvector)
-                expect(@continuous).to be_an_instance_of(Array)
-                expect(@continuous[0][:points]).to eq(87)
-                expect(@continuous.length).to eq(287)
-            end
-
-            # it '' do
-            # end
-
-            # it '' do
-            # end
+    describe 'process data for barcharts and histograms' do
+        before :each do
+            @file_path = File.join Rails.root, 'spec/fixtures/files/wine-data.csv'
+            @dataframe = Daru::DataFrame.read_csv(@file_path, liberal_parsing: true)
         end
+
+        it 'convert continuous vector to json' do
+            @columnvector = @dataframe["points"]
+            @continuous = Datacrunch.process_continous(@columnvector)
+            expect(@continuous).to be_an_instance_of(Array)
+            expect(@continuous[0][:points]).to eq(87)
+            expect(@continuous.length).to eq(287)
+        end
+
+        it 'categorical data with more than 10 unique values' do
+            @columnvector = @dataframe["province"]
+            @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
+            expect(@categorical).to be_an_instance_of(Array)
+            expect(@categorical[0][:freq]).to eq(82)
+            expect(@categorical[0][:column]).to eq("California")
+            expect(@categorical[-1][:freq]).to eq(102)
+            expect(@categorical[-1][:column]).to eq("Other values")
+            expect(@categorical.length).to eq(11)
+        end
+
+        it 'categorical data with missing values' do
+            @columnvector = @dataframe["region_2"]
+            @categorical = Datacrunch.process_categorical(@dataframe, @columnvector)
+            expect(@categorical).to be_an_instance_of(Array)
+            expect(@categorical[-2][:freq]).to eq(184)
+            expect(@categorical[-2][:column]).to eq(:null)
+            expect(@categorical.length).to eq(11)
+        end
+
     end
 end
